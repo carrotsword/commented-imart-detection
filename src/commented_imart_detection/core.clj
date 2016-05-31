484f16cde0c149b5a45b00c72f8a226e65511545
carrotsword <carrotsword@gmail.com>
2016-05-31 20:20:26 +0900

    ちょっとした修正

--- src/commented_imart_detection/core.clj
+++ src/commented_imart_detection/core.clj
@@ -2,10 +2,13 @@
   (:use [clojure.pprint]
         [spyscope.core]))
 
+(defrecord NumberedLine [number data])
+
+(defrecord ModifiedLine [start end state data line-number])
+
 (defn line-stator
-  [line-and-number]
-  (let [line (last line-and-number)
-        line-number (first line-and-number)
+  [numbered-line]
+  (let [line (:data numbered-line)
         start (.indexOf line "<!--")
         end (.indexOf line "-->")
         line-state (cond
@@ -14,11 +17,16 @@
                      (= end -1) :start
                      (< start end) :start-end
                      (< end start) :end-start )]
-    {:start start
-     :end (+ 3 end)
-     :state line-state
-     :line line
-     :line-number line-number}))
+    (ModifiedLine.
+      start
+      (if (= end -1) -1 (+ 3 end))
+      line-state
+      line
+      (:number numbered-line))))
+
+(defn modify-lines
+  [lseq]
+  (map line-stator (map (fn [a b] (NumberedLine. a b)) (iterate inc 0) lseq)))
 
 
 (defn transit
@@ -58,56 +66,70 @@
       :end-start :start
       new-state)))
 
-;;; start, end, continue, start/end, end/start, off
+;;; start, end, continue, start/end, end/start, off, on
 
 (def initial-result { :comment ""
                       :comments []
                       :line-number 0
                       :state :off })
 
-(defn comment-extractor
-  [current line-state]
-  (let [new-status (transit (:state current) (:state line-state))
-        line (:line line-state)
-        start (:start line-state)
-        end (:end line-state)
-        comments (:comments current)]
-    (case  new-status
-      :off current
-      :on        (assoc current
-                   :comment (str (:comment current) line ))
-      :start     (assoc current
-                   :comment (subs line start)
-                   :line-number (:line-number line-state))
-      :end       (assoc current
-                   :comment ""
-                   :comments (conj comments [(:line-number current) (str (:comment current) (subs line 0 end))]))
-      :start-end (assoc current
-                   :comments (conj comments [(:line-number line-state)(subs line start end)]))
-      :end-start (assoc current
-                   :comment (subs line start)
-                   :comments (conj comments [(:line-number current)(str (:comment current) (subs line 0 end))])
-                   :line-number (:line-number line-state))
-      initial-result )))
+(defn conj-nl
+  [comments line data]
+  (conj comments (NumberedLine. line data)))
+
+(defn collect-comments
+  [current modified-line]
+  (let [line (:data modified-line)
+        start (:start modified-line)
+        end (:end modified-line)
+        cmt (:comment current)
+        comments (:comments current)
+        current-no (:line-number current)
+        newline-no (:line-number modified-line)
+        new-state (transit (:state current) (:state modified-line))]
+    (assoc (case  new-state
+             :off       current
+             :on        (assoc current
+                          :comment (str cmt line ))
+             :start     (assoc current
+                          :comment (subs line start)
+                          :line-number newline-no)
+             :end       (assoc current
+                          :comment ""
+                          :comments (conj-nl comments current-no (str cmt (subs line 0 end))))
+             :start-end (assoc current
+                          :comments (conj-nl comments newline-no (subs line start end)))
+             :end-start (assoc current
+                          :comment (subs line start)
+                          :comments (conj-nl comments current-no (str cmt (subs line 0 end)))
+                          :line-number newline-no)
+             initial-result )
+      :state new-state)))
+
+(defrecord Result [file numbered-line])
 
 (defn contains-imart?
-  [line-comment]
-  (let [string (last line-comment)]
+  [numbered-line]
+  (let [string (.toLowerCase (:data numbered-line))]
     (or (<= 0 (.indexOf string "<imart"))
         (<= 0 (.indexOf string "</imart"))
         (<= 0 (.indexOf string "imart>")))))
 
-(defn detector
+(defn extract-html-comment
+  [lseq]
+  (:comments (reduce collect-comments initial-result (modify-lines lseq))))
+
+(defn extract-imart-comment
+  [lseq]
+    (filter contains-imart? (extract-html-comment lseq)))
+
+(defn extract-imart-comment-from-file
   [file]
   (with-open [r (clojure.java.io/reader file)]
-    (let [extract-result (reduce comment-extractor initial-result (map line-stator (map vector (iterate inc 0) (doall (line-seq r)))))
-          comments (:comments extract-result)]
-      (map (fn [item] {:file (.getAbsolutePath file)
-              :line-number (first item)
-              :comment (last item)})
-           (filter contains-imart? comments)))))
+    (map (fn [item] (Result. (.getAbsolutePath file) item))
+         (extract-imart-comment (doall (line-seq r))))))
 
-(defn html-filter
+(defn  html?
   [item]
   (and (.exists item) (.isFile item) (.endsWith (.toLowerCase (.getName item)) ".html")))
 
@@ -117,4 +139,8 @@
 
 (defn -main
   [x]
-  (clojure.pprint/pprint (filter not-empty? (map detector (filter html-filter (file-seq (clojure.java.io/file x)))))))
+  (clojure.pprint/pprint
+    (filter not-empty?
+            (map extract-imart-comment-from-file
+                 (filter html?
+                         (file-seq (clojure.java.io/file x)))))))
